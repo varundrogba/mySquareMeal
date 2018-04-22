1fbd5945709ea78952000a982d063cd40e4379d6
Varun Shah <varundrogba@gmail.com>
2018-04-21 18:03:03 -0700

    json object parsing

--- 295 Project/js/mapview.js
+++ 295 Project/js/mapview.js
@@ -1,14 +1,20 @@
 var locations = [
-    ['Bondi Beach', -33.890542, 151.274856, 4],
-    ['Coogee Beach', -33.923036, 151.259052, 5],
-    ['Cronulla Beach', -34.028249, 151.157507, 3],
-    ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-    ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ['Bondi Beach', -33.890542, 151.274856],
+    ['Coogee Beach', -33.923036, 151.259052],
+    ['Cronulla Beach', -34.028249, 151.157507],
+    ['Manly Beach', -33.80010128657071, 151.28747820854187],
+    ['Maroubra Beach', -33.950198, 151.259302]
 ];
+
+$.getJSON("http://localhost:63342/295%20Project/api/response.json?", function(data) {
+    $.each(data, function(i, f) {
+        locations.push([f.restaurant_name, f.address.latitude, f.address.longitude]);
+    });
+});
 
 var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
-    center: new google.maps.LatLng(-33.92, 151.25),
+    center: new google.maps.LatLng(37.328255, -121.883693),
     mapTypeId: google.maps.MapTypeId.ROADMAP
 });
 
